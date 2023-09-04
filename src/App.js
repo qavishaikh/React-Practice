5b96153fe7c025bacc38cf7b7adb7e078b293999
Qavi <qavi.shaikh11@gmail.com>
2023-09-04 14:56:02 +0500

    State Practice.. useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
+
 function App() {
+  let [data, setData] = useState("Hello World");
+
+  const updateData = () => {
+    setData("Qavi Shaikh");
+    // alert(data);
+  }
+
+  console.log("Render......")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{data}</h1>
+     <h2>{data}</h2>
+     <h3>{data}</h3>
+     <h4>{data}</h4>
+     <h5>{data}</h5>
+     <h6>{data}</h6>
+
+     <button onClick={updateData}>Click Me..</button>
     </div>
   );
 }
