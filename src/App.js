be3506ef6628043f8845786b005f0b77dfa3bb65
Qavi <qavi.shaikh11@gmail.com>
2023-09-07 17:01:35 +0500

    Form Validate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React  from 'react';
 import './App.css'
-import Condition from './Condition';
+import FormValidate from './FormValidate';
 
 const App = () => {
   return (
     <div className='App'>
-      <h1>Condiotional Statements</h1>
-      <Condition />
+      <h1>Form Validate</h1>
+      <FormValidate />
     </div>
   )
 }
